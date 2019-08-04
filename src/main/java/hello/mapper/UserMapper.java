4a37b62a6414a4413f6c52f2b6f2bf1b949507e6
huangjiaxi <trustfor@sina.cn>
2019-08-04 22:25:10 +0800

    登录连接mysql

--- src/main/java/hello/mapper/UserMapper.java
+++ src/main/java/hello/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package hello.mapper;
 
-import hello.service.User;
+import hello.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -9,4 +10,11 @@ import org.apache.ibatis.annotations.Select;
 public interface UserMapper {
     @Select("SELECT * FROM user WHERE id = #{id}")
     User findUserById(@Param("id") Integer id);
+
+    @Select("SELECT * FROM user WHERE username = #{username}")
+    User findUserByUsername(@Param("username") String username);
+
+    @Insert("insert into user(username,encrypted_password,created_at,updated_at) " +
+            "values(#{username},#{encrypted_password},now(),now())")
+    void save(@Param("username") String username,@Param("encrypted_password")  String encrypted_password );
 }
--- src/main/java/hello/service/UserService.java
+++ src/main/java/hello/service/UserService.java
@@ -1,6 +1,7 @@
 package hello.service;
 
 import hello.entity.User;
+import hello.mapper.UserMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,8 +9,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -17,34 +16,28 @@ public class UserService implements UserDetailsService {
     // 密码加密器
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    // ConcurrentHashMap 是线程安全的 HashMap 线程不安全
-    private Map<String,User> users = new ConcurrentHashMap<>();
+    private UserMapper userMapper;
 
-    public UserService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserService(BCryptPasswordEncoder bCryptPasswordEncoder,UserMapper userMapper) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userMapper = userMapper;
         save("aaa","111");
     }
 
     public void save(String username, String password){
-        users.put(username,new User(1,username,bCryptPasswordEncoder.encode(password)));
+        userMapper.save(username,bCryptPasswordEncoder.encode(password));
     }
 
-//    public User getUserById(Integer id){
-//        return null;
-//    }
-
     public User getUserByUsername(String username){
-        return users.get(username);
+        return userMapper.findUserByUsername(username);
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if(!users.containsKey(username)){
+        User user = getUserByUsername(username);
+        if(user == null){
             throw new UsernameNotFoundException( username + "不存在!");
         }
-
-        // 获取的是加密后的密码
-        User user = users.get(username);
 
         return new org.springframework.security.core.userdetails.User(username,user.getEncryptedPassword(), Collections.emptyList());
     }
