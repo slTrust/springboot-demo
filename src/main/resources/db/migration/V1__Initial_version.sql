d838346b4e0ee00fe186fd81a3a0fc5a4d9e1cc7
huangjiaxi <trustfor@sina.cn>
2019-09-02 21:31:51 +0800

    add flyway 实现数据库迁徙 和修改 pom 依赖的测试插件

---
+++ src/main/resources/db/migration/V1__Initial_version.sql
@@ -0,0 +1,8 @@
+create table user(
+  id bigint primary key auto_increment,
+  username varchar(10),
+  encrypted_password varchar(100),
+  avatar varchar(100),
+  created_at datetime,
+  updated_at datetime
+)
